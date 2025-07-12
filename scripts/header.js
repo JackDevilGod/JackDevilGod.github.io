8e10ebd85134d8779941a4a3b82676cdc81919f9
JackDevilGod <jack.jh666@gmail.com>
2025-07-12 07:08:07 +0200

    maved the javascript ffor header import into shared js script

--- scripts/header.js
+++ scripts/header.js
@@ -3,3 +3,9 @@ console.log("✅ mobile_menu.js loaded");
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 };
+
+fetch('/static_elements/nav_bar.html')
+                .then(response => response.text())
+                .then(data => {
+                    document.getElementById('navbar-container').innerHTML = data;
+                });
