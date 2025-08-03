8f16077ac553a98d93702ad8d6e4adcfaad033a5
JackDevilGod <jack.jh666@gmail.com>
2025-08-03 10:43:04 +0200

    made components folder

--- scripts/header.js
+++ scripts/header.js
@@ -2,13 +2,13 @@ function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 };
 
-fetch('/static_elements/nav_bar.html')
+fetch('/components/nav_bar.html')
                 .then(response => response.text())
                 .then(data => {
                     document.getElementById('navbar-container').innerHTML = data;
                 });
 
-fetch('/static_elements/nav_list.html')
+fetch('/components/nav_list.html')
                 .then(response => response.text())
                 .then(data => {
                     document.getElementById('desktop_menu').innerHTML = data;
