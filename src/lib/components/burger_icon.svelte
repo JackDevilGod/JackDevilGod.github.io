d46a08113e6c1cbecc176ab24ef8a8aa3069e9a7
JackDevilGod <jack.jh666@gmail.com>
2025-08-06 15:01:39 +0200

    added basic burger

---
+++ src/lib/components/burger_icon.svelte
@@ -0,0 +1,31 @@
+<script>
+	const amount = [1, 2, 3];
+</script>
+
+
+<div id='burger'>
+	{#each  amount as id}
+		<div id='bar{id}' class='burger_layer'></div>
+	{/each}
+</div>
+
+<style>
+	.burger_layer {
+        width: 35px;
+        height: 3px;
+
+        background-color: white;
+        margin: 6px 0;
+        transition: 0.4s;
+    }
+
+	#burger {
+		height: 100%;
+		width: auto;
+
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { MediaQuery } from 'svelte/reactivity';
+	import BurgerIcon from '$lib/components/burger_icon.svelte';
 
 	import dg_logo from '$lib/assets/logo_dg.png'
 
@@ -10,8 +10,10 @@
 		{route: '/', name: 'something'}
 		];
 	
-	function toggleMobileMenu(menu) {
-		menu.classList.toggle('open');
+	let open = false;
+
+	let onClick = () => {
+		open = !open;
 	};
 
 </script>
@@ -36,17 +38,9 @@ development hell.</pre>
 		</ul>
 	</nav>
 
-	<div id="hamburg_icon">
-		<div class="b1"></div>
-		<div class="b1"></div>
-		<div class="b1"></div>
-
-		<ul id='mobile_menu'>
-			{#each main_pages as {route, name}}
-				<li><a href="{route}">{name}</a></li>
-			{/each}
-		</ul>
-	</div>
+	<nav id='mobile_navbar'>
+		<BurgerIcon/>
+	</nav>
 </header>
 
 {@render children?.()}
@@ -101,7 +95,7 @@ development hell.</pre>
 		background-color: #dddddd;
 	}
 
-	nav{
+	#desktop_navbar{
 		display: flex;
 
 		ul {
@@ -135,83 +129,23 @@ development hell.</pre>
 		} 
 	}
 
-	#hamburg_icon {
-		height: 100%;
-
+	#mobile_navbar {
 		display: none;
 
-		cursor: pointer;
-
-		div {
-			width: 35px;
-			height: 3px;
-
-			background-color: white;
-			margin: 6px 0;
-			transition: 0.4s;
-		}
-	}
-
-	.open .b1 {
-		-webkit-transform: rotate(-45deg) translate(-6px, 6px);
-		transform: rotate(-45deg) translate(-6px, 6px);
-	}
-
-	#mobile_menu {
-		width: 200px;
-		max-height: 0;
-		overflow: hidden;
-		transition: max-height 0.4s ease, opacity 0.2s ease;
-		height: auto;
-		top: 100px;
-		right: 0;
-		left: auto;
-		position: absolute;
-		align-self: flex-end;
-		background-color: #080808;
-		opacity: 0;
-		pointer-events: none;
-		box-shadow: -2px 2px 10px rgba(0,0,0,0.2);
-		border-radius: 0 0 0 10px;
-
-		li {
-			width: 100%;
-			text-align: center;
-			margin-bottom: 10px;
-			a {
-				display: block;
-				width: 100%;
-				padding: 12px 0;
-				color: #f7f7f7;
-				text-decoration: none;
-				box-sizing: border-box;
-			}
-		}
-	}
-
-	.open #mobile_menu {
-	max-height: 500px;
-	opacity: 1;
-	pointer-events: auto;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: flex-start;
+		margin-right: 20px;
 	}
 
 	@media only screen and (max-width: 1150px) {
+		#desktop_navbar {
+			display: none;
+		}
+
 		.header_text {
 			font-size: large;
 		}
 
-		header nav {
-			display: none;
-		}
-
-		#hamburg_icon {
+		#mobile_navbar {
 			display: block;
 		}
-
 	}
-
 </style>
