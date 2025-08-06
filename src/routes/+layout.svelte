ef5fff5e56c920a8ff8b7c3b6e574926d56cb23c
JackDevilGod <jack.jh666@gmail.com>
2025-08-06 13:50:10 +0200

    added skelteon of mobile menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { MediaQuery } from 'svelte/reactivity';
+
 	import dg_logo from '$lib/assets/logo_dg.png'
 
 	let { children } = $props();
@@ -7,6 +9,11 @@
 		{route: '/', name: 'Home'},
 		{route: '/', name: 'something'}
 		];
+	
+	function toggleMobileMenu(menu) {
+		menu.classList.toggle('open');
+	};
+
 </script>
 
 <svelte:head>
@@ -14,20 +21,32 @@
 </svelte:head>
 
 <header>
-    <div >
-        <img src="{dg_logo}" alt="DG Logo">
-        <pre>
+	<div >
+		<img src="{dg_logo}" alt="DG Logo">
+		<pre class='header_text'>
 A page in
 development hell.</pre>
-    </div>
+	</div>
 
-    <nav>
+	<nav id='desktop_navbar'>
 		<ul>
 			{#each main_pages as {route, name}}
 				<li><a href="{route}">{name}</a></li>
 			{/each}
 		</ul>
-    </nav>
+	</nav>
+
+	<div id="hamburg_icon">
+		<div class="b1"></div>
+		<div class="b1"></div>
+		<div class="b1"></div>
+
+		<ul id='mobile_menu'>
+			{#each main_pages as {route, name}}
+				<li><a href="{route}">{name}</a></li>
+			{/each}
+		</ul>
+	</div>
 </header>
 
 {@render children?.()}
@@ -73,43 +92,79 @@ development hell.</pre>
 	}
 
 	img {
-    	width: auto;
-    	height: 75%;
-    	border-radius: 5px;
+		width: auto;
+		height: 75%;
+		border-radius: 5px;
 
-    	float: left;
+		float: left;
 
-    	background-color: #dddddd;
+		background-color: #dddddd;
 	}
 
-	ul {
-		height: auto;
-		max-width: 1150px;
-		align-items: center;
-
-		vertical-align: middle;
-
+	nav{
 		display: flex;
-		justify-content: space-around;
 
-		list-style: none;
+		ul {
+			height: auto;
+			max-width: 1150px;
+			align-items: center;
 
-		font-size: xx-large;
+			vertical-align: middle;
 
-		li {
-			padding: 5px;
+			display: flex;
+			justify-content: space-around;
 
-			margin-inline: 10px;
+			list-style: none;
+
+			font-size: xx-large;
+
+			li {
+				padding: 5px;
+
+				margin-inline: 10px;
+			}
+
+			a {
+				color: #dddddd;
+				text-decoration: none;
+			}
+
+			li:hover {
+				background-color: rgb(51, 51, 51);
+			}
+		} 
+	}
+
+	#hamburg_icon {
+		margin: auto 0;
+
+		display: none;
+
+		cursor: pointer;
+
+		div {
+			width: 35px;
+			height: 3px;
+
+			background-color: white;
+			margin: 6px 0;
+			transition: 0.4s;
+		}
+	}
+
+	@media only screen and (max-width: 1150px) {
+		.header_text {
+			font-size: large;
 		}
 
-		a {
-			color: #dddddd;
-			text-decoration: none;
+		header nav {
+			display: none;
 		}
 
-		li:hover {
-			background-color: rgb(51, 51, 51);
+		#hamburg_icon {
+			display: block;
 		}
-	} 
+
+	}
 
 </style>
