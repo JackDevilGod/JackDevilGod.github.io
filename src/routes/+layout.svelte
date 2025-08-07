1d132da6439401ee2a32370ac38ba48ba2629ef5
JackDevilGod <jack.jh666@gmail.com>
2025-08-07 16:24:27 +0200

    something

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-	export const trailingSlash = 'always';
-	export const prerender = true;
-
 	import BurgerIcon from '$lib/components/burger_icon.svelte';
 
 	import dg_logo from '$lib/assets/logo_dg.png'
---
+++ src/routes/+layout.ts
@@ -0,0 +1,20 @@
+// if you want to generate a static html file
+// for your page.
+// Documentation: https://kit.svelte.dev/docs/page-options#prerender
+export const prerender = true;
+
+//If you want to Generate a SPA
+//You have to set ssr to false.
+// This is not the case (so set it as true or comment on the line)
+// Documentation: https://kit.svelte.dev/docs/page-options#ssr
+export const ssr = true;
+
+// How to manage the trailing slashes in the URLs
+// By default, SvelteKit strips trailing slashes,
+// meaning that a request for /about/ will result in a redirect to /about.
+//The URL for the about page will be /about with 'never' (default)
+//The URL for the about page will be /about/ with 'always'
+//The URL for the about page will be /about/ or /about with 'ignore' (not reccomended)
+// https://kit.svelte.dev/docs/page-options#trailingslash
+export const trailingSlash = 'never';
+
