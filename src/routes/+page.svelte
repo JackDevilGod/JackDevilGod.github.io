36d8f75a51bbdcc520ea5081929867eb5feb6ab1
JackDevilGod <jack.jh666@gmail.com>
2025-08-07 15:19:31 +0200

    imported home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit 5</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="content">
+            <h1 class="page_header">Welcome to a webpage!</h1>
+
+            <p>
+                This will just be a small crappy static website for some kind of use. 
+                currently using no framework or any industry standards. 
+            </p>
+
+            <pre>
+Currently a uni student going into the third year of study. 
+This website will thus be a collection of things I will do and have done as a portfolio.</pre>
+
+            <p>
+                I do a technical study of artificial intelligence, but I like to mix creative and technical things.
+                The other things I do are art, music and 3d printing.
+            </p>
+
+            <p>
+                So just check something out on this website and fuck off.
+            </p>
+</div>
