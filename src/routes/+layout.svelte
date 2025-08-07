2c668b23662908b331db2e1c1ebb998724159db4
JackDevilGod <jack.jh666@gmail.com>
2025-08-07 16:08:56 +0200

    added things

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	export const trailingSlash = 'always';
+	export const prerender = true;
+
 	import BurgerIcon from '$lib/components/burger_icon.svelte';
 
 	import dg_logo from '$lib/assets/logo_dg.png'
