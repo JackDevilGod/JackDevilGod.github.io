94665fd5a8c29ba932d87765eaf53bbb7c64ff66
JackDevilGod <jack.jh666@gmail.com>
2025-08-09 03:08:31 +0200

    error works

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,6 +2,14 @@
 	import { page } from '$app/state';
 </script>
 
-<h1>You encountered {page.status}</h1>
+<h1>You encountered {page.status} error</h1>
 
-<h2>meaning {page.error.message}</h2>
+<h2>{page.error.message}</h2>
+
+<style>
+	* {
+		color: crimson;
+		font-size: 5rem;
+		text-shadow: 3rem;
+	}
+</style>
