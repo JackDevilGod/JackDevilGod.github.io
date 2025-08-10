43b17755c08a7052977200a4289d969b53c40b43
JackDevilGod <jack.jh666@gmail.com>
2025-08-10 15:02:27 +0200

    made grid for social table

---
+++ src/lib/components/social_table.svelte
@@ -0,0 +1,18 @@
+<script>
+
+    let {height = 500} = $props()
+</script>
+<div>
+    <h1>1</h1>
+    <h2>2</h2>
+</div>
+
+<style>
+    div {
+        width: 100%;
+        height: 100%;
+
+        display: grid;
+        grid-template-columns: auto auto auto;
+    }
+</style>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,48 +1,23 @@
+<script>
+	import SocialTable from "$lib/components/social_table.svelte";
+
+</script>
+
+
 <div class="content">
     <h1 class="page_header">this is the contact page.</h1>
 
-    <table class="contact_table">
-        <thead>
-            <tr>
-                <th>Platform</th>
-                <th>Usage</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td><a target="_blank" href="mailto:devilgods.696@gmail.com">Email</a></td>
-                <td>Use for official business and such.</td>
-            </tr>
-        </tbody>
-        <tbody>
-            <tr>
-                <td><a target="_blank" href="https://discord.gg/3xvuqWCfuu">Discord</a></td>
-                <td>For chatting and hanging around.</td>
-            </tr>
-        </tbody>
-    </table>
+    <div id='social_table'>
+        <SocialTable />
+    </div>
 </div>
 
 <style>
-    .contact_table {
-        width: 95%;
+    #social_table {
+        width: 50%;
+        height: 500px;
 
-        background-color: rgb(19, 19, 19);
-        border-collapse: collapse;
-
-        * {
-            border-bottom: 2px solid #ccc;
-        }
-
-        th {
-            text-align: left;
-
-            font-size: xx-large;
-        }
-
-        a {
-            text-decoration: none;
-            color: rgb(43, 43, 204);
-        }
+        margin-left: auto;
+        margin-right: auto;
     }
 </style>
