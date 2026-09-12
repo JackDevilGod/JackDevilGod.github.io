221cddb1c59a6856afade160396654fbd44e8f42
JackDevlGod <jack.jh666@gmail.com>
2026-09-12 15:42:43 +0200

    changed text to the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,11 @@
 <div class="content">
-	<h1 class="page_header">Welcome to a webpage!</h1>
+	<h1 class="page_header">Welcome to Heaven and Hell.</h1>
 
 	<p>
-		This will just be a small crappy static website for some kind of use. currently using Svelte.
+	    You, why are you here? Well I guess you should know me already from somewhere. 
 	</p>
-
-	<pre>
-Currently a uni student going into the third year of study. 
-This website will thus be a collection of things I will do and have done as a portfolio.</pre>
 
 	<p>
-		I do a technical study of artificial intelligence, but I like to mix creative and technical
-		things. The other things I do are art, music and 3d printing.
+	    Welp, just explore around I guess and see what more you can learn, but hey no one is going to read this any way.
 	</p>
-
-	<p>So just check something out on this website and fuck off.</p>
 </div>
